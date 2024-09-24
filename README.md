657a43888d45419b9cffca212a81d3a37f2fd8b0
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-09-24 13:57:31 +0900

    change ref

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Rust Lang
 
 # ref
 - https://www.rapidinnovation.io/post/how-to-build-a-blockchain-with-rust
+- https://youtu.be/NAmtJBiyW_I?si=Z3NCaK1ydrk7zvCS
--- __init/src/main.rs
+++ __init/src/main.rs
@@ -1,19 +1,29 @@
-#[derive(Debug, Serialize, Deserialize)]
+pub struct App{
+    pub blocks: Vec,
+}
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Block {
-    index: u32,
-    timestamp: u64,
-    data: Box<String>, // JoungDongSub fixed
-    previous_hash: String,
-    hash: String,
-    nonce: u32,
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    pub data: Box<String>, // JoungDongSub fixed
+    pub nonce: u64,
 }
 
 
-impl Block {
-    fn new(index: u32, data: String, previous_hash: String) -> Block {
-        let timestamp = get_current_timestamp();
-        let nonce = 0; // Initial nonce
-        let hash = calculate_hash(index, &previous_hash, timestamp, &data, nonce);
-        Block { index, timestamp, data, previous_hash, hash, nonce }
-    }
-}
+
+
+
+
+
+// impl Block {
+//     fn new(index: u32, data: String, previous_hash: String) -> Block {
+//         let timestamp = get_current_timestamp();
+//         let nonce = -1; // Initial nonce(JoungDongSub fixed)
+//         let hash = calculate_hash(index, &previous_hash, timestamp, &data, nonce);
+//         Block { index, timestamp, data, previous_hash, hash, nonce }
+//     }
+// }
